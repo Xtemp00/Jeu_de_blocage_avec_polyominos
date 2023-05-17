97ef078dfcb95f59056a765f972690a3aad3f4d8
AKKA Maroan <132614304+Yukiooh@users.noreply.github.com>
2023-05-17 16:14:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+/**
+ * @author Benjamin Rondo
+ * @author Maroan AKKA 
+ * @date 17 Mai 2023
+ * @version 1.8
+ * Github du projet : https://github.com/Xtemp00/Jeu_de_blocage_avec_polyominos
+ */
+
 public class Main {
     public static void main(String[] args) {
         Jeu jeu = new Jeu();
